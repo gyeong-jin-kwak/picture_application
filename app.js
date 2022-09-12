879580bef01daa62b9947b4abd2ddbebabd35958
곽경진 <kgj1212_@naver.com>
2022-09-12 20:42:04 +0900

    [test] canvas의 함수 기능 test

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("hello")
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+// canvas 크기설정
+// css로 크기 설정을 하지 않는 이유: 좋은 화질, 이미지를 위해서
+canvas.width = 800;
+canvas.height = 800;
+
+// canvas.getContext("2d")
+context.fillRect(10, 10, 100, 300);
