5b7f566a092211ff245002d03e674b97a558a529
곽경진 <kgj1212_@naver.com>
2022-09-13 09:19:55 +0900

    [test] rect, fill, fillStyle, beginPath

--- app.js
+++ app.js
@@ -6,5 +6,19 @@ const context = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
+///////////////////////////////////////
 // canvas.getContext("2d")
-context.fillRect(10, 10, 100, 300);
+// context.fillRect(10, 10, 100, 300);
+///////////////////////////////////////
+
+//////////////////////////////////////
+context.rect(50, 50, 100, 100);
+context.rect(150, 150, 100, 100);
+context.rect(250, 250, 100, 100);
+context.fill();
+
+context.beginPath();
+context.rect(350, 350, 100, 100);
+context.fillStyle = "red";
+context.fill();
+
